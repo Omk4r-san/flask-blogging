5201d524f4e068fa413a4e21a35cce8201a1c240
Omk4r-san <74708985+Omk4r-san@users.noreply.github.com>
2020-12-31 23:48:36 +0530

    Database

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,23 +1,41 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField,PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, length, EqualTo
+from wtforms.validators import DataRequired, length, EqualTo, ValidationError
 from email_validator import validate_email, EmailNotValidError
+from flaskblog.models import User
+
+#Registration form inputs
 
 class RegistrationForm(FlaskForm):
     username = StringField('username', validators=[DataRequired(), length(min = 3 , max=100)])
     email = StringField('email', validators=[DataRequired()])
+
+    # Validate email.
+
     try:
-  # Validate.
        valid = validate_email(" @ ")
        email = valid.email
     except EmailNotValidError as e:
-       print(str(e))
+       pass
 
   
     password = PasswordField('password', validators=[DataRequired()])
     confirm_password = PasswordField('confirm_password', validators= [DataRequired(), EqualTo('password')])
     submit = SubmitField('sign up')
 
+#vaidate username
+
+    def validate_username(self, username):
+       user = User.query.filter_by(username=username.data).first()
+       if user:
+          raise ValidationError('That username is already taken. Please use another username')
+
+#validate email
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+           raise ValidationError('That email is already taken. Please use another email')
 
 class LoginForm(FlaskForm):
    
